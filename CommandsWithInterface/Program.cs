e9c9f75d230690eabcce4e06fb4e16e447f1b42d
GET Academy <terje@kolderup.net>
2024-06-06 11:33:23 +0200

    x

---
+++ CommandsWithInterface/Program.cs
@@ -0,0 +1,18 @@
+﻿using CommandsWithInterface;
+
+var todoApp = new TodoApp();
+while (true)
+{
+    Console.Clear();
+    todoApp.Show();
+    Console.WriteLine(@"Hva vil du gjøre? 
+1 = legge til ny oppgave
+2 = markere oppgave som gjort");
+    var cmdNo = Console.ReadLine();
+    if (cmdNo == "1")
+    {
+    }
+    else if (cmdNo == "2")
+    {
+    }
+}
